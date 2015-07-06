1eeeb4ce1902eebe047e1f4da782065c40d4bde0
Bippity <alex.waang@yahoo.com>
2015-07-06 01:07:34 -0500

    Api version tick 1.18

--- AFK/AFK.cs
+++ AFK/AFK.cs
@@ -12,7 +12,7 @@ using System.ComponentModel;
 
 namespace AFK
 {
-    [ApiVersion(1, 17)]
+    [ApiVersion(1, 18)]
     public class AFK : TerrariaPlugin
     {
         public static AFKConfigFile AFKConfig { get; set; }
